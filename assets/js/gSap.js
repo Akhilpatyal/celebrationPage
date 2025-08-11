1cd13ff853fffba303e2c909beedd200cd5f166d
akhil <akhilpatyal49@gmail.com>
2025-08-11 15:15:49 +0530

    add new gallery and remove errors

--- assets/js/gSap.js
+++ assets/js/gSap.js
@@ -1,34 +1,3 @@
-// First, wrap each character in a <span>
-
-// heading function
-// function heading(){
-
-
-// const heading = document.querySelector("#heading");
-// const chars = heading.textContent.split("");
-
-// heading.innerHTML = chars.map(char => {
-//   const safeChar = char === " " ? "&nbsp;" : char;
-//   return `<span class="char">${safeChar}</span>`;
-// }).join("");
-
-// // Now animate with GSAP
-// gsap.set(".char", { opacity: 0, y: 20 });
-
-// gsap.to(".char", {
-//   opacity: 1,
-//   y: 0,
-//   stagger: 0.08,
-//   duration: 4,
-//   ease: "power2.out",
-//   scrollTrigger: {
-//     trigger: "#heading",
-//     start: "top 80%",
-//     toggleActions: "play none none reverse"
-//   }
-// });
-
-// }
 function heading(){
   const heading = document.querySelector("#heading");
   const chars = heading.textContent.split("");
@@ -53,7 +22,6 @@ function heading(){
   });
 }
 
- 
 heading();
 
 // cursor magnet
@@ -84,7 +52,4 @@ window.gsap.registerPlugin(
 	window.TweenMax
 );
 
-
 // 
-
-
